a5610eb400a35fbd0692937862f3f02ba2081126
User <user@example.com>
2026-03-23 11:27:27 +0800

    自动更新博客内容

--- auto_update_blog.sh
+++ auto_update_blog.sh
@@ -4,7 +4,7 @@
 cd /Users/zzw868/PycharmProjects/zzw868.github.io || exit
 
 echo "拉取远程最新代码..."
-git pull origin master
+git pull origin main
 
 echo "添加所有修改..."
 git add .
@@ -13,6 +13,6 @@ echo "提交修改（自动提交）..."
 git commit -m "自动更新博客内容"
 
 echo "推送到远程仓库..."
-git push origin master
+git push origin main
 
 echo "博客已成功更新并推送到GitHub！"
